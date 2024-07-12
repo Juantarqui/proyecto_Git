b556d2ae9ff323c61e6bd6f05d26d8411f0a4be0
juancarlostarqui <tarquiblanco4@gmail.com>
2024-07-12 17:53:21 -0400

    carpeta src de java

---
+++ src/casting/Casting.java
@@ -0,0 +1,24 @@
+package casting;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//casting
+		int a = 1, b = 3;
+		float c = 3.1f;
+		int s = a + (int)c;
+		boolean aa = true;
+		System.out.println(s);
+		//noooo
+		//int ss = aa + (int))c ;
+		
+		//parseo de datos
+		String nombre = "23";
+		float sss = 2 + Float.parseFloat(nombre);
+		System.out.print(sss);
+		
+	
+	}
+
+}
